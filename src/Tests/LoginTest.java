3387b2bad6f707b4b402fea4470cf9481190d72e
HAiL <hail@MacBook-Pro-HAiL.local>
2020-02-21 20:21:23 +0200

    hz

--- src/Tests/LoginTest.java
+++ src/Tests/LoginTest.java
@@ -2,6 +2,12 @@ import generic.ChromeTestCase;
 import org.testng.annotations.Test;
 import steps.LoginSteps;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -11,6 +17,21 @@ public class LoginTest extends ChromeTestCase {
     public void newTest() {
         LoginSteps loginSteps = new LoginSteps();
         loginSteps.openLoginPage();
+        try (InputStream input = new FileInputStream("TestData/UserAccounts.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("db.url"));
+            System.out.println(prop.getProperty("db.user"));
+            System.out.println(prop.getProperty("db.password"));
+
+        } catch (IOException  ex) {
+            ex.printStackTrace();
+        }
         loginSteps.login("igorbugun@gmail.com", "goftho39");
         assertEquals(driver.getCurrentUrl(), "http://www.dirtmixer.com");
     }

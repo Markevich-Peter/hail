91195c4ca86c3d0369e87df91db9e10710a2d75c
Markevich-Peter <mpeterivanov@gmail.com>
2020-02-27 14:40:46 +0300

    Easy mod

--- src/Tests/RegistrationTest.java
+++ src/Tests/RegistrationTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import generic.ChromeTestCase;
+import org.testng.annotations.Test;
+import steps.LoginSteps;
 
-public class RegistrationTest {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import static org.testng.Assert.assertEquals;
+
+import static com.thoughtworks.selenium.SeleneseTestNgHelper.assertEquals;
+
+public class RegistrationTest extends ChromeTestCase
+{
+ @Test                                                               //Сам тест
+    public void registrationTest()
+ {}
+
 }
